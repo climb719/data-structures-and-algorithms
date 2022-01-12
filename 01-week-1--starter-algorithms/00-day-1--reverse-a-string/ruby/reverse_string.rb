d529ca55154d0a4b5d8d1b2f827152c53a91d3ad
climb719 <mary.oldweiler@gmail.com>
2022-01-12 10:54:44 -0500

    ruby solutions

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,6 +1,11 @@
 def reverse_string(str)
-  # type your code in here
+  reversed = ""
+  str.chars.each do |char|
+    reversed = char + reversed
+  end
+  reversed
 end
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
@@ -11,8 +16,31 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
 
-  # Don't forget to add your own!
+  #Don't forget to add your own!
+
+  puts
+
+  puts "Expecting: 'M'"
+  puts reverse_string('M')
+
+  puts
+
+  puts "Expecting: '' (empty string)"
+  puts reverse_string('')
+
 end
 
 # Please add your pseudocode to this file
+# initialize an empty string (reversed) that will be the expected result I want to returh 
+# iterate over each letter of the sting imputed in as the argument
+# place each character in the initialized string before the previous one placed
+# return reversed
+
 # And a written explanation of your solution
+
+# Rewrite the problem in your own words
+# Write a method that takes in a string as an argument and use iteration to output the string with all the letters in reverse.
+# Validate that you understand the problem
+# Write your own test cases
+# Pseudocode
+# Code!
--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,5 +1,11 @@
 def find_first_duplicate(arr)
-  # type your code in here
+  hash_count = Hash.new(0)
+
+  arr.each do |e|
+    hash_count[e] = hash_count[e] += 1
+    return e if hash_count[e] > 1
+  end
+  return -1
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +17,34 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
 
+  
+  puts
+
+  puts "Expecting: -1"
+  puts find_first_duplicate([])
+
+  puts
+
+  puts "Expecting: -1"
+  puts find_first_duplicate([7])
+
+
   # Don't forget to add your own!
 end
+#given an array, find the furst duplicate that occurs and return it.
 
 # Please add your pseudocode to this file
+# create a counter hash that keeps track of values
+# pass in default of 0 so can add on to the hash and increment a counter
+# iterate over the array and call counter hash with each element passed in as key
+# and increment as soon as find a duplicate
+# once there is a value in hash, return that key (dupliucate element)
+# else return -1
+
+
+
 # And a written explanation of your solution
+# iterate through the arr passed in as an argumnet passing in each element as a key and value of 1
+# when the incrementer hits a repeated value hash[e] = hash[e], it will increment and then give a value greater than one. Can then return the value 
+
+
